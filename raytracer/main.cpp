fcfa632aa9e1036dd2536217269d9f763367702c
Arthur Brainville <ybalrid@ybalrid.info>
2018-04-02 04:15:19 +0200

    Add plane
    Signed-off-by: Arthur Brainville (Ybalrid) <ybalrid@ybalrid.info>

--- raytracer/main.cpp
+++ raytracer/main.cpp
@@ -4,6 +4,7 @@
 #include <math.h>
 #include "Scene.hpp"
 #include "Sphere.hpp"
+#include "Plane.hpp"
 
 #define M_TAU 2 * M_PI
 #define RIGHT_ANGLE M_TAU / 4
@@ -22,6 +23,7 @@ int main()
 	s.addObject<Sphere>(Vector3d(0, 0, 4), 1);
 	s.addObject<Sphere>(Vector3d(2, 0, 4), 0.25);
 	s.addObject<Sphere>(Vector3d(-1, 0, 6), 2);
+	s.addObject<Plane>(Vector3d(0, 0, 5), Vector3d(0, 1, -1));
 	auto lamp = std::make_unique<Lamp>();
 
 	lamp->position.y = 1;
@@ -32,7 +34,6 @@ int main()
 
 	lamp		= std::make_unique<Lamp>();
 	lamp->value = 0.5;
-
 	s.addLamp(std::move(lamp));
 
 	s.render();
